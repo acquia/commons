e23fef9b1c85f111b43dfa23f12eda9c476e6535
mikestefanello <mikestefff@gmail.com>
2010-07-21 17:12:10 +0000

    Theme tweak - removed JS from login block

--- docroot/sites/all/themes/acquia_commons/block-header-user.tpl.php
+++ docroot/sites/all/themes/acquia_commons/block-header-user.tpl.php
@@ -2,9 +2,6 @@
 // $Id: block.tpl.php 5639 2009-12-24 01:14:40Z chris $
 ?>
 
-<?php $skinr.=' fusion-horiz-login';
-drupal_add_js(drupal_get_path('theme', 'fusion_core').'/js/jquery.overlabel.js', 'theme'); ?>
-
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?> <?php print $block_zebra; ?> <?php print $position; ?> <?php print $skinr; ?>">
   <div class="inner clearfix">
     <?php if (isset($edit_links)): ?>
