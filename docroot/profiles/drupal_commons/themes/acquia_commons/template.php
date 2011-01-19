50560f73eae93413ca594d09185e753588baee17
mikestefanello <mikestefff@gmail.com>
2011-01-19 09:13:29 -0500

    Fixed #1031338: Display post information not following theme settings

--- docroot/profiles/drupal_commons/themes/acquia_commons/template.php
+++ docroot/profiles/drupal_commons/themes/acquia_commons/template.php
@@ -15,45 +15,50 @@ function acquia_commons_breadcrumb($breadcrumb) {
  * Node preprocessing
  */
 function acquia_commons_preprocess_node(&$vars) {
-  // Load the node author
-  $author = user_load($vars['node']->uid);
+  // Only build custom submitted information if it was first available
+  // If it's not, that indicates that it's been turned off for this
+  // node type
+  if ($vars['submitted']) {
+    // Load the node author
+    $author = user_load($vars['node']->uid);
   
-  // Author picture
-  $picture = theme_imagecache('user_picture_meta', $author->picture ? $author->picture : variable_get('user_picture_default', ''), $author->name, $author->name);
-  $submitted = $author->uid ? l($picture, "user/{$author->uid}", array('html' => TRUE)) : $picture;
+    // Author picture
+    $picture = theme_imagecache('user_picture_meta', $author->picture ? $author->picture : variable_get('user_picture_default', ''), $author->name, $author->name);
+    $submitted = $author->uid ? l($picture, "user/{$author->uid}", array('html' => TRUE)) : $picture;
     
-  // Author information
-  $submitted .= '<span class="submitted-by">';
-  $submitted .= t('Submitted by !name', array('!name' => theme('username', $author)));
-  $submitted .= '</span>';
-    
-  // User points
-  if ($author->uid && module_exists('userpoints')) {
-    $points = userpoints_get_current_points($author->uid);
-    $submitted .= '<span class="userpoints-value" title="' . t('!val user points', array('!val' => $points)) . '">';
-    $submitted .= "({$points})"; 
+    // Author information
+    $submitted .= '<span class="submitted-by">';
+    $submitted .= t('Submitted by !name', array('!name' => theme('username', $author)));
     $submitted .= '</span>';
-  }
     
-  // User badges
-  if ($author->uid && module_exists('user_badges')) {
-    if (is_array($author->badges)) {
-      foreach ($author->badges as $badge) {
-        $badges[] = theme('user_badge', $badge, $author);
+    // User points
+    if ($author->uid && module_exists('userpoints')) {
+      $points = userpoints_get_current_points($author->uid);
+      $submitted .= '<span class="userpoints-value" title="' . t('!val user points', array('!val' => $points)) . '">';
+      $submitted .= "({$points})"; 
+      $submitted .= '</span>';
+    }
+    
+    // User badges
+    if ($author->uid && module_exists('user_badges')) {
+      if (is_array($author->badges)) {
+        foreach ($author->badges as $badge) {
+          $badges[] = theme('user_badge', $badge, $author);
+        }
+      }
+    
+      if (!empty($badges)) {
+        $submitted .= theme('user_badge_group', $badges);
       }
     }
     
-    if (!empty($badges)) {
-      $submitted .= theme('user_badge_group', $badges);
-    }
+    // Created time
+    $submitted .= '<span class="submitted-on">';
+    $submitted .= format_date($vars['node']->created);
+    $submitted .= '</span>';
+    
+    $vars['submitted'] = $submitted;
   }
-    
-  // Created time
-  $submitted .= '<span class="submitted-on">';
-  $submitted .= format_date($vars['node']->created);
-  $submitted .= '</span>';
-    
-  $vars['submitted'] = $submitted;
 }
 
 /**
