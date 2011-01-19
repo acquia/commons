2f76dd8b535280c1ae48d1169cb84ac0e93a47e3
mikestefanello <mikestefff@gmail.com>
2011-01-19 13:09:41 -0500

    Fixed comment theme to allow for full-width without text overlap

--- docroot/profiles/drupal_commons/themes/acquia_commons/comment.tpl.php
+++ docroot/profiles/drupal_commons/themes/acquia_commons/comment.tpl.php
@@ -3,44 +3,44 @@
 ?>
 
 <div class="comment <?php print $comment_classes;?> clear-block">
-<div class="comment-info">  
- <?php if ($comment->new): ?>
-  <a id="new"></a>
-  <span class="new"><?php print $new ?></span>
-  <?php endif; ?>
-  <?php if (!$comment->picture && (variable_get('user_picture_default', '') != '')):
-  $comment->picture =  variable_get('user_picture_default', '');
-  endif;
-  if ($comment->picture){
-$picture = theme_imagecache('user_picture_meta', $comment->picture, $name, $name);
-  print('<a href="'.$BASE_URL.'/user/'.$comment->uid.'" >'.$picture.'</a>');
-  }
- ?>
+
+  <div class="comment-info">  
+    <?php if ($comment->new): ?>
+      <a id="new"></a>
+      <span class="new"><?php print $new ?></span>
+    <?php endif; ?>
+    <?php 
+      if (!$comment->picture && (variable_get('user_picture_default', '') != '')) {
+        $comment->picture =  variable_get('user_picture_default', '');
+      }
+      if ($comment->picture) { 
+        $picture = theme_imagecache('user_picture_meta', $comment->picture, $name, $name);
+        print('<a href="'.$BASE_URL.'/user/'.$comment->uid.'" >'.$picture.'</a>');
+      }
+    ?>
     <div class="submitted">
-    <?php print $submitted ?>
-  </div>
+      <?php print $submitted ?>
+    </div>
   
     <?php if ($links): ?>
-  <div class="links">
-    <?php print $links ?>
-  </div>
-  <?php endif; ?>
-  </div>
- <div class="comment-content">
-  
-  <h3 class="title"><?php print $title ?></h3>
-
-  
-  <div class="content">
-    <?php print $content ?>
-    
-    <?php if ($signature): ?>
-    <div class="signature">
-      <?php print $signature ?>
-    </div>
+      <div class="links">
+        <?php print $links ?>
+      </div>
     <?php endif; ?>
   </div>
+ 
+  <div class="comment-content-wrapper">
+    <div class="comment-content">
+      <h3 class="title"><?php print $title ?></h3>
+      <div class="content">
+        <?php print $content ?>
+        <?php if ($signature): ?>
+          <div class="signature">
+            <?php print $signature ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
   </div>
-
   
 </div><!-- /comment -->
