ab55e475f0046350a828ab06ff9dd99b7e27e09b
Andrew Young <aycodes@gmail.com>
2012-08-06 10:12:59 -0500

    re-commit of files

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+output_style = :expanded
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+line_comments = true
