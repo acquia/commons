69e934bac31c6972a6baa6763d2506f5d9dca38e
Andrew Young <aycodes@gmail.com>
2012-08-13 15:15:45 -0500

    adding link to a css file that will temporary give us the site width until we figure out how to set the defaults for AT

--- templates/html/html.tpl.php
+++ templates/html/html.tpl.php
@@ -63,6 +63,7 @@
 <?php print $styles; ?>
 <link rel="stylesheet" href="<?php print base_path() . drupal_get_path('theme', 'commons_origins'); ?>/css/global.base.css">
 <link rel="stylesheet" href="<?php print base_path() . drupal_get_path('theme', 'commons_origins'); ?>/css/global.styles.css">
+<link rel="stylesheet" href="<?php print base_path() . drupal_get_path('theme', 'commons_origins'); ?>/css/responsive.desktop.css">
 <?php print $scripts; ?>
 <?php print $polyfills; ?>
 </head>
