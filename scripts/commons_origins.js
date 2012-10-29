59288bdfcbb5930e7961a4a91b999b73363b484d
chris.rochford <peterochford@gmail.com>
2012-10-29 11:04:58 -0500

    Main nav icons and hover/active arrow

--- scripts/commons_origins.js
+++ scripts/commons_origins.js
@@ -4,7 +4,7 @@
 
 	//placeholder functionality	
 		$('.view-id-commons_homepage_content .views-row-1 article h1').append("<div class='tag new'></div><div class='tag featured'></div>");
-
+		$('#block-system-main-menu .menu-depth-1 a').append("<div class='arrow'></div>")
 	});
 
 })(jQuery);
