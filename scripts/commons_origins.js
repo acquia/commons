b7a7d7e45382b7c4c834c566127e5422a4f99f60
chris.rochford <peterochford@gmail.com>
2012-10-29 09:57:41 -0500

    Rename images, add theme JS file, style featured/new content tags

---
+++ scripts/commons_origins.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+	$(document).ready(function() {
+
+	//placeholder functionality	
+		$('.view-id-commons_homepage_content .views-row-1 article h1').append("<div class='tag new'></div><div class='tag featured'></div>");
+
+	});
+
+})(jQuery);
