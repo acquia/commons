76b3013adf1116a38023c5eb3d1c4b356f282aea
chris.rochford <peterochford@gmail.com>
2012-11-14 13:47:04 -0600

    Exposed filters - show button on first select list change

--- scripts/commons_origins.js
+++ scripts/commons_origins.js
@@ -1,6 +1,21 @@
 (function ($) {
-
 	$(document).ready(function() {
+
+//		$.fn.textWidth = function(){
+//		  var html_org = $(this).html();
+//		  var html_calc = '<span>' + html_org + '</span>';
+//		  $(this).html(html_calc);
+//		  var width = $(this).find('span:first').width();
+//		  $(this).html(html_org);
+//		  return width;
+//		};
+	
+//		var selectwidth = $('#edit-following option:selected').textWidth();
+//		console.log(selectwidth);
+
+		$('.views-exposed-widgets .form-select').change(function() {
+			$('.views-exposed-widgets .views-submit-button').fadeIn(300);
+		});
 
 	//placeholder functionality	
 		$('.view-id-commons_homepage_content .views-row-1 article h1').append("<div class='tag new'></div><div class='tag featured'></div>");
