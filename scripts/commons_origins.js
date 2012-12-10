b7c50a81987027ffb3730bb819834c83b3bc11db
Andrew Young <aycodes@gmail.com>
2012-12-10 12:36:11 -0600

    adding styles for choose all posts widget; js for choose all posts widget

--- scripts/commons_origins.js
+++ scripts/commons_origins.js
@@ -23,4 +23,12 @@ jQuery(document).ready(function($){
   //placeholder functionality
   // $('.view-id-commons_homepage_content .views-row-1 article h1').append("<div class='tag new'></div><div class='tag featured'></div>");
   $('#block-system-main-menu .menu-depth-1 a').append("<div class='arrow'></div>");
+
+  $('.commons-bw-create-choose').click(function(){
+    $('body').addClass('create-choose-open');
+  });
+
+  $('.commons-bw-create-choose-bg').click(function(){
+    $('body').removeClass('create-choose-open');
+  });
 });
