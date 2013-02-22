026133f49c569e69a8774431346e6e563097ce00
Al Steffen <al@zarabadoo.org>
2013-02-22 14:25:49 -0500

    Issue #1917662 by Zarabadoo: Hide unwanted node links.

--- template.php
+++ template.php
@@ -151,7 +151,28 @@ function commons_origins_preprocess_node(&$vars) {
     $comments_link['attributes']['class'][] = 'link-with-counter';
     $comments_link['title'] = str_replace($vars['comment_count'], '<span class="counter">' . $vars['comment_count'] . '</span>', $comments_link['title']);
   }
+
+  // Hide some of the node links.
+  if (!empty($vars['content']['links'])) {
+    $hidden_links = array(
+      'node' => array(
+        'node-readmore',
+      ),
+      'comment' => array(
+        'comment-add',
+        'comment-new-comments'
+      ),
+    );
+    foreach ($hidden_links as $element => $links) {
+      foreach ($links as $link) {
+        if (!empty($vars['content']['links'][$element]['#links'][$link])) {
+          $vars['content']['links'][$element]['#links'][$link]['#access'] = FALSE;
+        }
+      }
+    }
+  }
 }
+/**
 function commons_origins_process_node(&$vars) {
 }
 // */
@@ -201,3 +222,86 @@ function commons_origins_preprocess_block(&$vars) {
 function commons_origins_process_block(&$vars) {
 }
 // */
+
+/**
+ * Overrides theme_links() for nodes.
+ *
+ * This allows for the theme to set a link's #access argument to FALSE so it
+ * will not render.
+ */
+function commons_origins_links($variables) {
+  $links = $variables['links'];
+  $attributes = $variables['attributes'];
+  $heading = $variables['heading'];
+  global $language_url;
+  $output = '';
+
+  if (count($links) > 0) {
+    $output = '';
+
+    // Treat the heading first if it is present to prepend it to the
+    // list of links.
+    if (!empty($heading)) {
+      if (is_string($heading)) {
+        // Prepare the array that will be used when the passed heading
+        // is a string.
+        $heading = array(
+          'text' => $heading,
+          // Set the default level of the heading.
+          'level' => 'h2',
+        );
+      }
+      $output .= '<' . $heading['level'];
+      if (!empty($heading['class'])) {
+        $output .= drupal_attributes(array('class' => $heading['class']));
+      }
+      $output .= '>' . check_plain($heading['text']) . '</' . $heading['level'] . '>';
+    }
+
+    $output .= '<ul' . drupal_attributes($attributes) . '>';
+
+    $num_links = count($links);
+    $i = 1;
+
+    foreach ($links as $key => $link) {
+      if ($link['#access'] !== FALSE) {
+        $class = array($key);
+
+        // Add first, last and active classes to the list of links to help out themers.
+        if ($i == 1) {
+          $class[] = 'first';
+        }
+        if ($i == $num_links) {
+          $class[] = 'last';
+        }
+        if (isset($link['href']) && ($link['href'] == $_GET['q'] || ($link['href'] == '<front>' && drupal_is_front_page())) && (empty($link['language']) || $link['language']->language == $language_url->language)) {
+          $class[] = 'active';
+        }
+        $output .= '<li' . drupal_attributes(array('class' => $class)) . '>';
+
+        if (isset($link['href'])) {
+          // Pass in $link as $options, they share the same keys.
+          $output .= l($link['title'], $link['href'], $link);
+        }
+        elseif (!empty($link['title'])) {
+          // Some links are actually not links, but we wrap these in <span> for adding title and class attributes.
+          if (empty($link['html'])) {
+            $link['title'] = check_plain($link['title']);
+          }
+          $span_attributes = '';
+          if (isset($link['attributes'])) {
+            $span_attributes = drupal_attributes($link['attributes']);
+          }
+          $output .= '<span' . $span_attributes . '>' . $link['title'] . '</span>';
+        }
+
+        $i++;
+        $output .= "</li>\n";
+      }
+    }
+
+    $output .= '</ul>';
+  }
+
+  return $output;
+}
