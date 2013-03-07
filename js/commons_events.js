e06942688ab92012a9bfc22d2e04d57985000b60
Al Steffen <al@zarabadoo.org>
2013-03-07 09:31:28 -0500

    Bugfix by Zarabadoo: Check there is data in the argument before operating on it.

--- js/commons_events.js
+++ js/commons_events.js
@@ -1,9 +1,11 @@
 (function ($) {
   $.fn.changeElementType = function(newType) {
     var attrs = {};
-    $.each(this[0].attributes, function(idx, attr) {
-      attrs[attr.nodeName] = attr.nodeValue;
-    });
+    if ($(this).length) {
+      $.each(this[0].attributes, function(idx, attr) {
+        attrs[attr.nodeName] = attr.nodeValue;
+      });
+    }
     this.replaceWith(function() {
       return $("<" + newType + "/>", attrs).append($(this).contents());
     });
