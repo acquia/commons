a784b276959e46abf914247ad3903dbf59be1d00
Al Steffen <al.steffen@acquia.com>
2013-03-13 09:28:16 -0400

    Cleanup by Zarabadoo: Fix comments to reference this theme.

--- templates/form/form.tpl.php
+++ templates/form/form.tpl.php
@@ -3,13 +3,9 @@
  * @file
  * Output a form.
  *
- * This is an override of Drupal's default theme_form() to make theme
- * hook suggestions and theming easier.
- *
  * @see theme_form()
- * @see pure_suggestions_form()
- * @see pure_preprocess_form()
- * @see pure_process_form()
+ * @see commons_origins_preprocess_form()
+ * @see commons_origins_process_form()
  */
 ?>
 <form<?php print $attributes; ?>>
