e19d7689d7cbdcef23b30a26ee2b4523f6ef5619
Al Steffen <al@zarabadoo.org>
2013-03-25 11:03:52 -0400

    Issue #1921502 by Zarabadoo: Restyle the rating widget to use all css and be more flexible with font-size changes.

--- commons-thumbs-up-down.tpl.php
+++ commons-thumbs-up-down.tpl.php
@@ -5,17 +5,19 @@
  */
 ?>
 
-<ul>
-  <li class="thumb-up">
-    <?php print $up_button; ?>
-  </li>
-  <li class="thumb-down">
-    <?php print $down_button; ?>
-  </li>
-</ul>
 <?php
-print '<div class="rate-info"><span class="rate-info-value">' . $results['rating'] . '</span> <span class="rate-info-label">' . format_plural($results['rating'], t('point'), t('points')) . '</span></div>';
+print '<div class="rate-info"><span class="rate-info-value">' . $results['rating'] . '</span> <span class="rate-info-label">' . format_plural($results['rating'], 'point', 'points') . '</span></div>';
 
 if ($display_options['description']) {
   print '<div class="rate-description">' . $display_options['description'] . '</div>';
 }
+?>
+
+<div class="commons-q-a-rate-buttons">
+  <div class="commons-q-a-rate-trigger commons-q-a-rate-up">
+    <?php print $up_button; ?>
+  </div>
+  <div class="commons-q-a-rate-trigger commons-q-a-rate-down">
+    <?php print $down_button; ?>
+  </div>
+</div>
