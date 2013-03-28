386768f3aeb6ec1bc0bffd6c3f17073a8da29dee
Al Steffen <al.steffen@acquia.com>
2013-03-28 11:41:44 -0400

    Issue #1955468 by Zarabadoo: Remove commented out functions and extraneous comments.

--- template.php
+++ template.php
@@ -3,24 +3,7 @@
 /**
  * @file
  * Process theme data.
- *
- * Use this file to run your theme specific implimentations of theme functions,
- * such preprocess, process, alters, and theme function overrides.
- *
- * Preprocess and process functions are used to modify or create variables for
- * templates and theme functions. They are a common theming tool in Drupal, often
- * used as an alternative to directly editing or adding code to templates. Its
- * worth spending some time to learn more about these functions - they are a
- * powerful way to easily modify the output of any template variable.
- *
- * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
- * 1. Rename each function and instance of "commons_origins" to match
- *    your subthemes name, e.g. if your theme name is "footheme" then the function
- *    name will be "footheme_preprocess_hook". Tip - you can search/replace
- *    on "commons_origins".
- * 2. Uncomment the required function to use.
  */
-
 
 /**
  * Implements hook_theme().
@@ -82,25 +65,11 @@ function commons_origins_preprocess_html(&$vars) {
     $vars['classes_array'][] = drupal_html_class($palette);
   }
 
-  // Two examples of adding custom classes to the body.
-
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
+  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome
+  // etc.
   $vars['classes_array'][] = css_browser_selector();
 
 }
-//
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function commons_origins_process_html(&$vars) {
-}
-// */
 
 /**
  * Implements theme_menu_link().
@@ -169,8 +138,6 @@ function commons_origins_preprocess_page(&$vars) {
 }
 function commons_origins_process_page(&$vars) {
 }
-// */
-
 
 /**
  * Override or insert variables into the node templates.
@@ -526,27 +493,6 @@ function commons_origins_css_alter(&$css) {
     unset($css['profiles/commons/modules/contrib/rich_snippets/rich_snippets.css']);
   }
 }
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function commons_origins_preprocess_comment(&$vars) {
-}
-function commons_origins_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function commons_origins_preprocess_block(&$vars) {
-}
-function commons_origins_process_block(&$vars) {
-}
-// */
 
 /**
  * Overrides theme_links() for nodes.
