20e02a6689be87e0d1c42d00defb282df6fa7a56
Joseph Pontani <jpontani@vt.edu>
2013-04-09 17:07:13 -0400

    Issue #1908778 by jpontani: allow switching of registration types, but only if there are no registrations already for the event; also fix the JS for updating summary of registration settings and topics

--- js/commons_events.js
+++ js/commons_events.js
@@ -12,33 +12,23 @@
   }
   Drupal.behaviors.commons_events_update_registration_settings_legend = {
     attach: function (context, settings) {
-      $("#edit-event-registration-settings .summary").changeElementType("div");
-      $("#edit-event-topics .summary").changeElementType("div");
-      if ($('#legend-registration-type').length == 0) {
-        $('#edit-event-registration-settings .summary').append("<span id='legend-registration-type'></span>");
-      }
-      if ($('#legend-registration-status').length == 0) {
-        $('#edit-event-registration-settings .summary').append("<span id='legend-registration-status'></span>");
-      }
-      if ($('#legend-event-topics').length == 0) {
-        $('#edit-event-topics .summary').append("<span id='legend-event-topics'></span>");
-      }
       $(':input[name^="field_topics"]').change(function() {
         if ($(':input[name^="field_topics"]').val() == "") {
-          $('#legend-event-topics').text("No topics");
+          $("[id^='edit-event-topics'] .summary").text("No topics");
         }
         else {
-          $('#legend-event-topics').text($(':input[name^="field_topics"]').val());
+          $("[id^='edit-event-topics'] .summary").text($(':input[name^="field_topics"]').val());
         }
       });
       $(':input[name^="field_registration_type"]').change(function() {
-        $('#legend-registration-type').text($(':input[name^="field_registration_type"] :selected').text());
+        $("[id^='edit-event-registration-settings'] .summary").text($(':input[name^="field_registration_type"] :selected').text());
         if ($(':input[name^="field_registration_type"]').val() == 'external') {
           $(':input[name^="field_status"]').val('0').change();
         }
       });
       $(':input[name^="field_status"]').change(function() {
-        $('#legend-registration-status').text($(':input[name="field_status"] :selected').text());
+        var cur = $(':input[name^="field_registration_type"] :selected').text();
+        $("[id^='edit-event-registration-settings'] .summary").text(cur + ', ' + $(':input[name="field_status"] :selected').text());
       });
       $(':input[name^="field_topics"]').change();
       $(':input[name^="field_registration_type"]').change();
