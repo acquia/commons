e5120137d0aa5cb1c1b43510322a5549f488c1e6
barisw <barisw@107229.no-reply.drupal.org>
2013-06-03 15:51:16 -0400

    Issue #1995508 by BarisW: add topics field dynamically to content that implements commons_entity_integration

--- js/commons_events.js
+++ js/commons_events.js
@@ -1,25 +1,6 @@
 (function ($) {
-  $.fn.changeElementType = function(newType) {
-    var attrs = {};
-    if ($(this).length) {
-      $.each(this[0].attributes, function(idx, attr) {
-        attrs[attr.nodeName] = attr.nodeValue;
-      });
-    }
-    this.replaceWith(function() {
-      return $("<" + newType + "/>", attrs).append($(this).contents());
-    });
-  }
   Drupal.behaviors.commons_events_update_registration_settings_legend = {
     attach: function (context, settings) {
-      $(':input[name^="field_topics"]').change(function() {
-        if ($(':input[name^="field_topics"]').val() == "") {
-          $("[id^='edit-event-topics'] .summary").text("No topics");
-        }
-        else {
-          $("[id^='edit-event-topics'] .summary").text($(':input[name^="field_topics"]').val());
-        }
-      });
       $(':input[name^="field_registration_type"]').change(function() {
         $("[id^='edit-event-registration-settings'] .summary").text($(':input[name^="field_registration_type"] :selected').text());
         if ($(':input[name^="field_registration_type"]').val() == 'external') {
@@ -30,7 +11,6 @@
         var cur = $(':input[name^="field_registration_type"] :selected').text();
         $("[id^='edit-event-registration-settings'] .summary").text(cur + ', ' + $(':input[name="field_status"] :selected').text());
       });
-      $(':input[name^="field_topics"]').change();
       $(':input[name^="field_registration_type"]').change();
       $(':input[name^="field_status"]').change();
     }
