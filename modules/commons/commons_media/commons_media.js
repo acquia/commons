43d86a86b103f146094e7cdf0bea18b950b4691e
Devin Carlson <devin@devincarlson.ca>
2014-03-28 15:51:14 -0400

    Hid the upload button when a file is not present.

--- modules/commons/commons_media/commons_media.js
+++ modules/commons/commons_media/commons_media.js
@@ -2,8 +2,17 @@
 
 Drupal.behaviors.autoUpload = {
   attach: function (context, settings) {
-    $('form', context).delegate('input.form-file', 'change', function() {
-      $(this).next('input[type="submit"]').mousedown();
+    // Loop through all of the managed file form elements on the page.
+    $('.form-managed-file').each(function(index, element) {
+      // Hide/show the file upload/remove button if a file is not present.
+      if ($(this).children('span.file').length == 0 ) {
+        $(this).children('input.form-submit').hide();
+      };
+
+      // Automatically upload the file if a file is selected.
+      $(this).delegate('input.form-file', 'change', function() {
+        $(this).siblings('input.form-submit').mousedown();
+      });
     });
   }
 };
