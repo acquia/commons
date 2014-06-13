2d4f7050780bad18607c0a7e452498011ee46af4
Devin Carlson <devin@devincarlson.ca>
2014-06-13 07:48:49 -0400

    Added custom browsing widget titles to each of the Commons content types.

--- themes/commons/commons_origins/template.php
+++ themes/commons/commons_origins/template.php
@@ -758,7 +758,10 @@ function commons_origins_preprocess_form_content(&$variables, $hook) {
   // theme hook suggestions for easy overriding.
   if (strpos($variables['form']['#form_id'], 'commons_bw_partial_node_form_') === 0) {
     $variables['form']['actions']['submit']['#attributes']['class'][] = 'action-item-primary';
-    $variables['form']['title']['#markup'] = str_replace('<h3>', '<h3 class="partial-node-form-title">', $variables['form']['title']['#markup']);
+
+    if (isset($variables['form']['title'])) {
+      $variables['form']['title']['#markup'] = '<h3 class="partial-node-form-title">' . $variables['form']['title']['#markup'] . '</h3>';
+    }
   }
 
   // Make the links and buttons on the private message forms have the
